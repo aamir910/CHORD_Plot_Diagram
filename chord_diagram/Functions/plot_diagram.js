041b054af69bded6e78c95d57118293084c79f4c
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-03-04 13:19:11 +0500

    update the style of the chord diagram

--- chord_diagram/Functions/plot_diagram.js
+++ chord_diagram/Functions/plot_diagram.js
@@ -2,8 +2,8 @@ function Chord_initials(matrix){
 
 
     
-    const width = 800;
-    const height = 800;
+    const width = 700;
+    const height = 700;
     const outerRadius = Math.min(width, height) * 0.5 - 30;
     const innerRadius = outerRadius - 70;
 
@@ -17,7 +17,7 @@ function Chord_initials(matrix){
     // Create the SVG
     const svg = d3.select("#chordDiagram")
         .append("svg")
-        .attr("viewBox", [-width / 2, -height / 2, width * 2, height * 2])
+        .attr("viewBox", [-width  , -height +200, width * 2, height * 2])
         .attr("width", "100%")
         .attr("height", "auto")
         .attr("font-size", 12)
@@ -54,25 +54,38 @@ chord2.forEach(chord => {
 
     // Add the arcs for the groups
     group.append("path")
-    .attr("fill", d => color(d.index))
-    .attr("stroke", d => d3.rgb(color(d.index)).darker())
+    .each((d, i) => {
+        console.log("before " , d);
+        d.bold = i >= 15; // Bold text from index 7 onwards
+        console.log("before " , d);
+    })
+    .attr("fill", d => d.bold ? "#000080" : color(d.index))
+    .attr("stroke", d => d.bold ? "#000080" : d3.rgb(color(d.index)).darker())
     .attr("d", d3.arc()
-        .innerRadius(innerRadius+5) // Inner radius of the arc
+        .innerRadius(innerRadius) // Inner radius of the arc
         .outerRadius(outerRadius) // Outer radius of the arc
         .startAngle(d => d.startAngle) // Start angle of the arc
         .endAngle(d => d.endAngle) // End angle of the arc
     );
+
     // Add the group labels
     group.append("text")
-        .each(d => (d.angle = (d.startAngle + d.endAngle) / 2))
-        .attr("dy", ".35em")
-        .attr("transform", d => `
-    rotate(${(d.angle * 180 / Math.PI - 90)})
-    translate(${innerRadius + 10})
-    ${d.angle > Math.PI ? "rotate(180)" : ""}
-`)
-        .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
-        .text((d, i) => categories[i]);
+    .each((d, i) => {
+        console.log("before " , d);
+        d.angle = (d.startAngle + d.endAngle) / 2;
+        d.bold = i >= 15; // Bold text from index 7 onwards
+        console.log("before " , d);
+    })
+    .attr("dy", ".35em") 
+    .attr("transform", d => `
+        rotate(${(d.angle * 180 / Math.PI - 90)})
+        translate(${innerRadius + 80})
+        ${d.angle > Math.PI ? "rotate(180)" : ""}
+    `)
+    .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+    .style("font-weight", d => d.bold ? "bold" : "normal")
+    .text((d, i) => categories[i]);
+
 
     // Add the chords
     svg.append("g")
